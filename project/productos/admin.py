64ab256e1272e9812294cc99c4a820931709b524
RAArrieta <alfredoarrieta99@gmail.com>
2024-06-11 17:29:16 -0300

    .

--- project/productos/admin.py
+++ project/productos/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import CategoriaProducto, Producto
+from .models import CategoriaProducto, Producto, Cliente, Pedido
 
-# Register your models here.
+class ProductoAdmin(admin.ModelAdmin):
+    # ORDENO EN ADMIN POR NOMBRE, PRECIO
+    list_display=("nombre", "precio", "precio_doc", "precio_media")
+    search_fields=("nombre",)
+    list_filter=("categoria",)
+    
+class PedidoAdmin(admin.ModelAdmin):
+    list_display=("numero", "fecha")
+    # FILTRAR POR FECHA
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+    
 
 admin.site.register(CategoriaProducto)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Cliente)
+admin.site.register(Pedido, PedidoAdmin)
--- project/productos/models.py
+++ project/productos/models.py
@@ -10,11 +10,24 @@ class CategoriaProducto(models.Model):
     
 class Producto(models.Model):
     categoria=models.ForeignKey(CategoriaProducto, on_delete=models.CASCADE)
-    nombre=models.CharField(max_length=50)
-    precio=models.IntegerField()
+    nombre=models.CharField(max_length=50, verbose_name="Producto")
+    precio=models.IntegerField(blank=True, null=True, verbose_name="Precio Unitario")
+    precio_doc=models.IntegerField(blank=True, null=True, verbose_name="Precio por docena")
+    precio_media=models.IntegerField(blank=True, null=True, verbose_name="Precio por mitad")
     
     def __str__(self):
         return self.nombre
 
+class Cliente(models.Model):
+    nombre=models.CharField(max_length=50)
+    email=models.EmailField()
     
+    def __str__(self):
+        return self.nombre
     
+class Pedido(models.Model):
+    numero=models.IntegerField(verbose_name="Número de Pedido") 
+    fecha=models.DateField(verbose_name="Fecha")
+    
+    def __str__(self):
+        return f"{self.numero}"
